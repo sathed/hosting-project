9c02d3b5ebdac59921361454b952d8a91a061f56
Clint <clint.pedersen@gmail.com>
2019-07-09 23:49:10 -0600

    Update index.js
    Making it Lambda independent.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
+var bucketName = 'mtech-lambda-project-bucket';
+var pi = "3.1415926535897932384626433";
 
-exports.handler = (event, context, callback) => {
-
-};
+// The keyName should be your name.
+var keyName = '<your_name>';
+var object = 'pi.txt';
