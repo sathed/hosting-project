99e74fce53355d4a674f80bb8e43d06a2a53c04a
Clint <clint.pedersen@gmail.com>
2019-07-10 07:42:08 -0600

    Update index.js
    Updates to the file.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
-var bucketName = 'mtech-lambda-project-bucket';
-var pi = "3.1415926535897932384626433";
-
-// The keyName should be your name.
-var keyName = '<your_name>';
-var object = 'pi.txt';
+var bucketName = '';
+var pi = '';
+var keyName = '';
+var object = '';
