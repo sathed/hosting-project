6cf0aaadf1cc95c18616677a4b7216f794115983
Clint <clint.pedersen@gmail.com>
2020-02-27 08:01:55 -0700

    Update app.js
    Updated to the new hosting project

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
 var bucketName = '';
-var pi = '';
 var keyName = '';
 var object = '';
+
+exports.handler = async (event, context) => {
+    // Determine the winning choice
+    // Rock loses to paper
+    // Paper
+  
+    // 
+};
